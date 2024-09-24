434f104163ec1163e3ff3d472703a9028277a342
ianian4302 <88609092+ianian4302@users.noreply.github.com>
2024-09-25 01:58:32 +0800

    Refactor login_page.dart to use Design.secondaryColor for app bar background

--- lib/page/login_page.dart
+++ lib/page/login_page.dart
@@ -1,11 +1,13 @@
+import 'package:eztalk/utilities/design.dart';
 import 'package:flutter/material.dart';
+import '../utilities/router.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Login Page')),
+      appBar: AppBar(title: const Text('Login Page'), backgroundColor: Design.secondaryColor),
       body: const Center(
         child: Text(
           'Login Page',
