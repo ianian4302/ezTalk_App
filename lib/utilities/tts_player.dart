9210747f2a3b788995ee428f63eeaf2b2d2eac51
ianian4302 <88609092+ianian4302@users.noreply.github.com>
2024-10-11 02:58:17 +0800

    Refactor TtfPlayer to TtsPlayer for consistency and clarity

--- lib/utilities/tts_player.dart
+++ lib/utilities/tts_player.dart
@@ -2,8 +2,11 @@ import 'package:flutter_tts/flutter_tts.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'settings.dart';
 
-class TtfPlayer {
+typedef CompletionCallback = void Function();
+
+class TtsPlayer {
   static FlutterTts? _flutterTts;
+  static CompletionCallback? _completionCallback;
 
   static Future<FlutterTts> getFlutterTts() async {
     if (_flutterTts == null) {
@@ -15,11 +18,17 @@ class TtfPlayer {
   static Future<void> initFlutterTts() async {
     _flutterTts = FlutterTts();
     await _loadSettings();
+    _flutterTts!.setCompletionHandler(() {
+      if (_completionCallback != null) {
+        _completionCallback!();
+      }
+    });
   }
 
   static Future<void> _loadSettings() async {
     final prefs = await SharedPreferences.getInstance();
-    String voice = prefs.getString('selectedVoice') ?? Settings.voices.keys.first;
+    String voice =
+        prefs.getString('selectedVoice') ?? Settings.voices.keys.first;
     String locale = Settings.voices[voice] ?? "zh-TW";
     double pitch = prefs.getDouble('pitch') ?? Settings.pitch;
     double rate = prefs.getDouble('rate') ?? Settings.rate;
@@ -29,8 +38,10 @@ class TtfPlayer {
     await _flutterTts!.setSpeechRate(rate);
   }
 
-  static Future<void> speak(String text) async {
+  static Future<void> speak(String text,
+      {CompletionCallback? onComplete}) async {
     final tts = await getFlutterTts();
+    _completionCallback = onComplete;
     await tts.speak(text);
   }
 
@@ -39,10 +50,12 @@ class TtfPlayer {
     await tts.stop();
   }
 
-  static Future<void> updateSettings({String? voice, double? pitch, double? rate}) async {
+  static Future<void> updateSettings(
+      {String? voice, double? pitch, double? rate}) async {
     final tts = await getFlutterTts();
     if (voice != null) {
-      await tts.setVoice({"name": voice, "locale": "zh-TW"});
+      String locale = Settings.voices[voice] ?? "zh-TW";
+      await tts.setVoice({"name": voice, "locale": locale});
     }
     if (pitch != null) {
       await tts.setPitch(pitch);
